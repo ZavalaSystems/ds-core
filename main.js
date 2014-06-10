58f07cd4b7bedcc34dea217fe66d71daac89a394
Brandon Keown <bkeown@blitz.io>
2014-06-10 17:57:23 -0500

    Example feature toggle default off.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-(function main(mach, cfg) {
+(function main(mach, _, cfg, toggle) {
     "use strict";
     var app = mach.stack();
 
     app.get("/", function (request) {
-        var host = cfg.templates.host(request);
-        return mach.json({
+        var host = cfg.templates.host(request),
+            version = toggle.getToggleOff(request, "feature.version") ?
+                        {version: "0.0.1"} :
+                        {};
+
+        return mach.json(_.merge(version, {
             consultant: {
                 list: host + "/consultant",
                 root: host + "/consultant/root",
                 find: host + "/consultant"
             }
-        });
+        }));
     });
 
     app = require("./consultant.js")(app);
@@ -18,5 +22,7 @@
     mach.serve(app, cfg.server.port);
 }(
     require("mach"),
-    require("./config.js")
+    require("lodash"),
+    require("./config.js"),
+    require("./lib/toggle.js")
 ));
