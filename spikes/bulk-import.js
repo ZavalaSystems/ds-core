bcccba8e26135d980fe81ac73f36784a705bb27e
Ryan Zeigler <r.zeigler@icloud.com>
2014-09-15 12:54:22 -0500

    Improve import
    Ensure the correct information is imported and skip orders and lineitems.

--- spikes/bulk-import.js
+++ spikes/bulk-import.js
@@ -34,8 +34,15 @@
                 var reps = slurpReps(repsFile);
                 // We can assume only 1 bp at this point
                 var creations = _.map(reps, function(rep) {
-                    return cypher.cypherToObj('create (c:Consultant {firstname: {firstname}, lastname: {lastname}, rep: {rep}})\
-                        -[:PERFORMED]->(cp:ConsultantPerformance)', {firstname: rep['first-name'], lastname: rep['last-name'], rep: rep['rep-id']});
+                    return cypher.cypherToObj('create (c:Consultant {firstname: {firstname}, lastname: {lastname}, rep: {rep}, rank: {rank}, joindate: {joindate}})\
+                        -[:PERFORMED]->(cp:ConsultantPerformance)',
+                        {
+                            firstname: rep['first-name'],
+                            lastname: rep['last-name'], 
+                            rep: rep['rep-id'], 
+                            rank: rep['rank'], 
+                            joindate: new Date(rep['joindate']).toISOString()
+                        });
                 });
                 return q.all(creations)
                     .then(function(results) {
@@ -55,43 +62,11 @@
                             (bp:BusinessPeriod) \
                             create (bp)-[r:DURING]->(meta) return r', {rep: rep['rep-id']});
                     } else {
-                        return cypher.cypherToObj('match (:Consultant {rep: {child}})-[:PERFORMED]->(down:ConsultantPerformance), \
-                            (:Consultant {rep: {parent}})-[:PERFORMED]->(up:ConsultantPerformance) \
-                            create (down)-[r:REPORTS_TO]->(up) return r', {child: rep['rep-id'], parent: rep['sponsor-num']});
+                        return cypher.cypherToObj('match (c:Consultant {rep: {child}})-[:PERFORMED]->(downline:ConsultantPerformance), \
+                            (p:Consultant {rep: {parent}})-[:PERFORMED]->(upline:ConsultantPerformance) \
+                            create (downline)-[r:REPORTS_TO]->(upline), (c)-[e:ENROLLED_BY]->(p) return r, e', {child: rep['rep-id'], parent: rep['sponsor-num']});
                     }
                 }))
-                .then(function(results) {
-                    return {
-                        consultants: results.consultant,
-                        bp: results.bp
-                    };    
-                });
-            })
-            .then(function(results) {
-                console.log('importing orders');
-                var orders = slurpOrders(ordersFile);
-                return q.all(_.map(orders, function(order) {
-                    return cypher.cypherToObj('match (:Consultant {rep: {rep}})-[:PERFORMED]->(cp:ConsultantPerformance) \
-                        create (o:Order {id: {orderid}, when: {when}})-[r:PLACED_BY]->(cp) return o, r',
-                        {rep: order['rep-num'], orderid: order['order-num'], when: new Date(order['date-created']).toISOString()});
-                }))
-            })
-            .then(function(results) {
-                console.log('importing line items');
-                var lineItems = slurpLineItems(lineItemsFile);
-                return q.all(_.map(lineItems, function(lineItem) {
-                    return cypher.cypherToObj('match (o:Order {id: {orderid}}) \
-                        create (li:LineItem {id: {lineid}, price: {price}, qty: {quantity}, status: {status}, product: {product}})-[r:PART_OF]->(o) \
-                        return r, li',
-                        {   
-                            orderid: lineItem['order-num'], 
-                            lineid: lineItem['id'],
-                            price: parseFloat(lineItem['price']) * 100,
-                            quantity: lineItem['quantity'], 
-                            status: lineItem['status'],
-                            product: lineItem['product-num']
-                        });
-                }));
             })
             .done(function(s) {
                 console.log('done');
