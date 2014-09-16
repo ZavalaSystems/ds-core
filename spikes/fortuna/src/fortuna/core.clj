a59972230eaa3fb5af17c1bc95db580d00cd342d
Brandon Keown <bkeown@blitz.io>
2014-09-15 19:31:07 -0500

    One step closer to clojure commissions.

--- spikes/fortuna/src/fortuna/core.clj
+++ spikes/fortuna/src/fortuna/core.clj
@@ -15,12 +15,39 @@
             (assoc m k (:data v))
             (assoc m k (map #(:data %) v)))))
 
-(defn- no-neo4j [m & ks]
-    (println (count m) ks)
-    (reduce get-data-sanely m ks))
+(defn- no-neo4j [& args]
+    (let [m (last args)
+          ks (drop-last args)]
+      (reduce get-data-sanely m ks)))
+
+(defn- sum-detail [obj]
+  (fn [detail-name]
+    (->> obj
+         :lineItems
+         (filter #(= detail-name (-> % :type)))
+         (map #(* (-> % :price) (-> % :qty)))
+         (reduce +))))
+
+(defn- calculate-pcv [obj]
+  (let [total-for (sum-detail obj)
+        retail (total-for "retail")
+        wineclub (total-for "wineclub")
+        gifts (total-for "gifts")
+        pcv (+ retail (* (/ 3 4) wineclub) (* (/ 1 2) gifts))]
+    (assoc obj :retail retail
+               :wineclub wineclub
+               :gifts gifts
+               :pcv pcv
+               :qualified (> pcv 25000))))
 
 (defn -main []
-  (json/pprint (let [result (c/cypher commission-query, {})
-        make-obj (partial zipmap (map keyword (:columns result)))]
-    (no-neo4j (first (map make-obj (:data result))) :lineItems :downline :consultant)))
+  (println (let [result (c/cypher commission-query, {})
+        headers (->> result :columns (map keyword))
+        make-obj (partial zipmap headers)]
+    (->> result
+         :data
+         (map make-obj)
+         (first)
+         (no-neo4j :lineItems :downline :consultant)
+         (calculate-pcv))))
   (shutdown-agents))
