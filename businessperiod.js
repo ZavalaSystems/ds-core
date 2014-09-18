abe133c3e220d81947097d9f3eb72161b4fa6341
Ryan Zeigler <r.zeigler@icloud.com>
2014-09-18 15:48:00 -0500

    Implement business period lookup.

--- businessperiod.js
+++ businessperiod.js
@@ -1,7 +1,12 @@
 /*jslint maxlen: 120
     unparam: true */
-module.exports = (function (R, bilby, mach, uri, response, request, bp) {
+module.exports = (function (R, bilby, q, mach, lens, uri, response, request, bp) {
     "use strict";
+
+    var env = bilby.environment(),
+        findLens = bilby.objectLens("find").compose(request.queryLens),
+        idLens = bilby.objectLens("id").compose(request.paramsLens),
+        decodeQueryDate = R.compose(bp.decodeDate, lens.get, findLens.run);
 
     function formatPeriod(blob) {
         var currentId = bp.currentIdLens.run(blob).getter,
@@ -18,20 +23,47 @@ module.exports = (function (R, bilby, mach, uri, response, request, bp) {
         };
     }
 
-    function current(req) {
+    function resolveCurrent(req) {
         return bp.getCurrent()
             .then(bp.linker(uri.absoluteUri(req))(formatPeriod))
             .then(mach.json)
             .catch(response.catcher);
     }
 
+    function resolveByDate(req) {
+        return decodeQueryDate(req).map(function (d) {
+            return bp.getByDate(d)
+                .then(bp.linker(uri.absoluteUri(req))(formatPeriod))
+                .then(mach.json)
+                .catch(response.catcher);
+        }).getOrElse(q.when(404));
+    }
+
+    function resolveById(req) {
+        return bp.getById(parseInt(idLens.run(req).getter, 10))
+            .then(bp.linker(uri.absoluteUri(req))(formatPeriod))
+            .then(mach.json)
+            .catch(response.catcher);
+    }
+
+    function close(req) {
+        return 500;
+    }
+
+    env = env.method("resolve", bilby.compose(lens.get, findLens.run), resolveByDate)
+        .method("resolve", bilby.constant(true), resolveCurrent);
+
     return function (app) {
-        app.get("/bp", current);
+        app.get("/bp", env.resolve);
+        app.get("/bp/:id", resolveById);
+        app.post("/bp/:id/close", close);
     };
 }(
     require("ramda"),
     require("bilby"),
+    require("q"),
     require("mach"),
+    require("./lib/lens"),
     require("./lib/uri"),
     require("./lib/response"),
     require("./lib/request"),
