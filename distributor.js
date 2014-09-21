8a2e1e12ae9cb5a579e59b2fc32985d57dd3a78a
Ryan Zeigler <r.zeigler@icloud.com>
2014-09-21 16:31:07 -0500

    Implement creation.

--- distributor.js
+++ distributor.js
@@ -1,13 +1,14 @@
 /*jslint maxlen: 120*/
 module.exports = (function (R, bilby, mach, m, uri, response, distributor) {
     "use strict";
-    var env = bilby.environment();
+    var env = bilby.environment(),
+        formatDistributor = R.compose(distributor.transformOffsetToDate, distributor.matched);
 
     function createFull(req) {
         /* Duplicate ids should be taken care of by constraints in configure-db */
-        return distributor.createDistributorCypher(distributor.transformDate(req.params))
+        return distributor.createDistributorCypher(distributor.transformDateToOffset(req.params))
             .then(m.first)
-            .then(distributor.linker(uri.absoluteUri(req))(distributor.matched))
+            .then(distributor.linker(uri.absoluteUri(req))(formatDistributor))
             .then(m.map(mach.json))
             .then(m.getOrElse(response.status.badRequest({})))
             .catch(response.catcher);
@@ -16,19 +17,27 @@ module.exports = (function (R, bilby, mach, m, uri, response, distributor) {
     function createPartial(req) {
         return distributor.createPartialCypher(req.params)
             .then(m.first)
-            .then(distributor.linker(uri.absoluteUri(req))(distributor.matched))
+            .then(distributor.linker(uri.absoluteUri(req))(formatDistributor))
             .then(m.map(mach.json))
             .then(m.getOrElse(response.status.badRequest({})))
             .catch(response.catcher);
     }
 
+    function upgradeDistributor(req) {
+        return 500;
+    }
+
     env = bilby.environment()
         .method("createDistributor", R.compose(distributor.isValidFull, R.prop("params")), createFull)
         .method("createDistributor", R.compose(distributor.isValidPartial, R.prop("params")), createPartial)
-        .method("createDistributor", R.alwaysTrue, R.always(response.status.badRequest({})));
+        .method("createDistributor", R.alwaysTrue, R.always(response.status.badRequest({})))
+        .method("upgradeDistributor", R.compose(distributor.isValidUpgrade, R.prop("params")), upgradeDistributor)
+        .method("upgradeDistributor", R.alwaysTrue, R.always(response.status.badRequest({})));
 
     return function (app) {
+        app.get("/distributor/:distributorID", env.getDistributor);
         app.post("/distributor", env.createDistributor);
+        app.post("/distributor/:distributorID/upgrade", env.upgradeDistributor);
         return app;
     };
 }(
