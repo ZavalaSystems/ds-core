afcad8b691e6626f3b37d81b04bd636c38882e93
Ryan Zeigler <r.zeigler@icloud.com>
2014-09-26 10:54:17 -0500

    Make jslint happy.

--- nuke.js
+++ nuke.js
@@ -1,11 +1,11 @@
-module.exports = (function (mach, toggle, cypher) {
+module.exports = (function (toggle, cypher) {
+    "use strict";
     return function (app) {
         app.post("/nuke", function (req) {
             if (process.env.NODE_ENV === "development" && toggle.getToggleOff(req, "thermonuclearwar")) {
                 return cypher.cypherToObj("match (d) optional match (d)-[r]-() delete d, r", {})
                     .then(function () {
-                        return cypher.cypherToObj("create (bp:BusinessPeriod {start: {start}}) return bp",
-                        {
+                        return cypher.cypherToObj("create (bp:BusinessPeriod {start: {start}}) return bp", {
                             start: Date.now()
                         });
                     }).then(function () {
@@ -17,16 +17,14 @@ module.exports = (function (mach, toggle, cypher) {
                             content: "The only winning move is not to play.\n"
                         };
                     });
-            } else {
-                return {
-                    status: 403,
-                    content: "How about a nice game of chess?\n"
-                };
             }
+            return {
+                status: 403,
+                content: "How about a nice game of chess?\n"
+            };
         });
     };
 }(
-    require("mach"),
     require("./lib/toggle"),
     require("./lib/neo4j/cypher")
 ));
