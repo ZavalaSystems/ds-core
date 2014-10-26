c58b59db6f9d1708f14252f677e76c9690b072f4
Ryan Zeigler <r.zeigler@icloud.com>
2014-10-26 10:17:45 -0500

    Dedup now handles line items.

--- spikes/deduplicate.js
+++ spikes/deduplicate.js
@@ -3,39 +3,58 @@ module.exports = (function (path, fs, q, bilby, R, cypher) {
     var orders = cypher.cypherToObj("match (o:Order) return o, id(o) as o_id;"),
         deleteOrderQuery = fs.readFileSync(path.join(__dirname, "order-delete.cql"), {encoding: "utf8"}),
         deleteOrder = cypher.cypherToObj(deleteOrderQuery),
+        lineItems = cypher.cypherToObj("match (li:LineItem)-[:PART_OF]->(o:Order) return li, id(li) as li_id, o"),
+        deleteLineItemQuery = fs.readFileSync(path.join(__dirname, "lineitem-delete.cql"), {encoding: "utf8"}),
+        deleteLineItem = cypher.cypherToObj(deleteLineItemQuery),
         order = R.prop("o"),
         orderData = R.compose(R.prop("data"), order),
         orderNodeID = R.prop("o_id"),
         orderID = R.compose(R.prop("id"), R.prop("data"), order),
+        lineItem = R.prop("li"),
+        lineItemData = R.compose(R.prop("data"), lineItem),
+        lineItemID = R.compose(R.prop("id"), lineItemData),
+        lineItemNodeID = R.prop("li_id"),
         groupLenGt1 = R.compose(R.gt(1), R.prop("length"), R.prop("1")),
-        mkDeleteParams = function (id) { return {orderNodeID: id}; },
-        serializeDelete = function (arr) {
+        // Produce params for a query that contains id(n)={nodeID} as the predicate for selecting deletions
+        mkDeleteParams = function (id) { return {nodeID: id}; },
+        serializeDelete = R.curry(function (fDelTask, arr) {
             return R.reduce(function (acc, params) {
                 return acc.then(function () {
-                    return deleteOrder(params);
+                    return fDelTask(params);
                 });
             }, q.when(), arr);
-        };
+        });
 
-    function main() {
-        var dropList = orders({})
-            .then(R.groupBy(orderID)) // Group by order id
+    /* Return a strong key for line items, i.e. order id and line item id together */
+    function lineItemKey(blob) {
+        return lineItemID(blob) + "-" + orderID(blob)
+    }
+
+    function deduplicate(label, fID, fNodeID, fDelTask, fData, listPromise) {
+        var toDelete = listPromise.then(R.groupBy(fID)) // Group by order id
             .then(R.toPairs) // Convert to pairs
             .then(R.filter(groupLenGt1)) // Filter out groups of size 1
             .then(R.map(R.prop("1"))) // Get just the list
             .then(R.map(R.tail)) // Drop the first element in each group (assumed as the canonical version)
             .then(R.flatten);
-        // Log the pending drops
-        console.log("Dropping");
-        dropList.then(R.map(orderData))
+        return toDelete.then(function (x) {
+                console.log("Deleting " + label);
+                return x;
+            })
+            .then(R.map(fData))
             .then(R.map(JSON.stringify))
             .then(R.forEach(console.log))
-            .then(R.always(dropList))
-            .then(R.map(orderNodeID)) // Get the neo4j internal node id for ever order to drop
+            .then(R.always(toDelete))
+            .then(R.map(fNodeID))
             .then(R.map(mkDeleteParams))
-            .then(serializeDelete)
-            .catch(function (err) {
-                console.log(err);
+            .then(serializeDelete(fDelTask))
+    }
+
+    function main() {
+        deduplicate("Duplicate Orders", orderID, orderNodeID, deleteOrder, orderData, orders({}))
+            .then(function () {
+                return deduplicate("Duplicate LineItems",
+                    lineItemID, lineItemNodeID, deleteLineItem, lineItemData, lineItems({}));
             })
             .done();
     }
